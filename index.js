2ec1ee19975dd687e4f725434e5114495f6b2e6b
Brandon Fries <brandonfries1993@gmail.com>
2019-06-03 01:41:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,8 @@ function dec(n) {
   return --n
 }
 function makeInt(n) {
-  return parseInt(n)
+  return parseInt(n),10)
 }
 function preserveDecimal(n) {
   return parseFloat(n)
-}
-function ('assumes base 10') {
-return parseInt(n)
 }
